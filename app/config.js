eafaa059562cb950bab9a9f50b6181f053179cba
Fernando <ffabbiano@fi.uba.ar>
2021-10-19 18:28:42 -0300

    Fix linter errors

--- app/config.js
+++ app/config.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-undef */
 module.exports = {
     port: process.env.PORT || 8080,
     log: {
-      error: process.env.LOG_ERROR == undefined || process.env.LOG_ERROR == 'true',
-      warn: process.env.LOG_WARN == undefined || process.env.LOG_WARN == 'true',
-      info: process.env.LOG_INFO == undefined || process.env.LOG_INFO == 'true',
-      debug: process.env.LOG_DEBUG == undefined || process.env.LOG_DEBUG == 'true',
+      error: process.env.LOG_ERROR == undefined || process.env.LOG_ERROR == "true",
+      warn: process.env.LOG_WARN == undefined || process.env.LOG_WARN == "true",
+      info: process.env.LOG_INFO == undefined || process.env.LOG_INFO == "true",
+      debug: process.env.LOG_DEBUG == undefined || process.env.LOG_DEBUG == "true",
     }
-}
+};
