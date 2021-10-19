06267c5a1f9c4f1a5a89cbcb903ca01a22e06a67
Fernando <ffabbiano@fi.uba.ar>
2021-10-18 21:14:39 -0300

    Reformat logs

--- app/utils/log.js
+++ app/utils/log.js
@@ -2,26 +2,39 @@ const { log } = require('../config')
 
 const logInfo = function(message){
   if (log.info){
-    console.log('Info: ' + message);
+    console.log('INFO: | ' + getDate() + " | " + message);
   }
 }
 
 const logDebug = function(message){
   if (log.debug){
-    console.log('Debug: ' + message);
+    console.log('DEBUG: | ' + getDate() + " | " + message);
   }
 }
 
 const logWarn = function(message){
   if (log.warn){
-    console.log('Warn: ' + message);
+    console.log('WARN: | ' + getDate() + " | " + message);
   }
 }
 
 const logError = function(message){
   if (log.error){
-    console.log('Error: ' + message);
+    console.log('ERROR: | ' + getDate() + " | " + message);
   }
+}
+
+const getDate = function(){
+    date = new Date()
+    
+    year = date.getFullYear()
+    month = date.getMonth() + 1
+    day = ("0" + date.getDate()).slice(-2)
+    hours = ("0" + date.getHours()).slice(-2)
+    minutes = ("0" + date.getMinutes()).slice(-2)
+    seconds = date.getSeconds()
+
+    return (year + "-" +  month + "-" +  day + " " + hours + ":" + minutes + ":" + seconds);
 }
 
 module.exports = {
