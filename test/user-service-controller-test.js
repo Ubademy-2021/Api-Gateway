ce84cb941b8b7d1eeb366c78c3b600b1d4ffcd07
Fernando <ffabbiano@fi.uba.ar>
2021-10-19 18:45:45 -0300

    Add initial test

---
+++ test/user-service-controller-test.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+var assert = require("assert");
+
+describe("testing", function() {
+
+    it("should assert testing", function() {
+      assert.equal("testing", "testing");
+    });
+  
+  });
