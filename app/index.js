a1bcd0c369c68ca31963c9b5d66093bc5be538dc
Fernando <ffabbiano@fi.uba.ar>
2021-10-20 14:08:32 -0300

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,5 +1,12 @@
 "use strict";
 
+// Datadog config: this line must come before importing any instrumented module.
+// eslint-disable-next-line no-unused-vars
+const tracer = require("dd-trace").init({
+  logInjection: true,
+  service: "api-gateway"
+});
+
 const path = require("path");
 const express = require("express");
 const userServiceController = require("./controllers/user-service-controller");
