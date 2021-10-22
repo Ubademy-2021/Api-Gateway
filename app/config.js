0814d5b8aee3d4837eefe3ed0d9c280e64a5de4d
Fernando <ffabbiano@fi.uba.ar>
2021-10-22 00:14:03 -0300

    Update use of config var

--- app/config.js
+++ app/config.js
@@ -7,5 +7,5 @@ module.exports = {
       info: process.env.LOG_INFO == undefined || process.env.LOG_INFO == "true",
       debug: process.env.LOG_DEBUG == undefined || process.env.LOG_DEBUG == "true",
     },
-    base_heroku_url: "https://ubademy-user-service.herokuapp.com"
+    base_user_service_url: process.env.BASE_USER_SERVICE_URL || "https://ubademy-user-service.herokuapp.com"
 };
