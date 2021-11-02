04c789c1fc8035ff89e3ec2b7f0b32b0d2620747
Fernando <ffabbiano@fi.uba.ar>
2021-11-01 22:14:20 -0300

    Update functions to check tokens

--- app/services/api-gateway-service.js
+++ app/services/api-gateway-service.js
@@ -8,40 +8,61 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
 
-exports.manageAuthToken = (headers) => {
+exports.manageAuthToken = (headers, callback) => {
     if (headers["firebase_authentication"]){
         logInfo("Checking Firebase token");
-        this.checkFirebaseToken(headers["firebase_authentication"]);
+        
+        this.checkFirebaseToken(headers["firebase_authentication"], function(userEmail, err){
+            if (err){
+                logError("Error while checking Firebase token");
+                callback(null, err);
+            }
+
+            // Successfull scenario
+            callback(userEmail, null);
+        });
+        
         return;
     }
     
     if (headers["facebook_authentication"]){
         logInfo("Checking Facebook token");
-        this.checkFacebookToken(headers["facebook_authentication"]);
+        
+        this.checkFacebookToken(headers["facebook_authentication"], function(userEmail, err){
+            if (err){
+                logError("Error while checking Facebook token");
+                callback(null, err);
+            }
+
+            // Successfull scenario
+            callback(userEmail, null);
+        });
+
         return;
     }
 };
 
-exports.checkFirebaseToken = (token) => { 
+exports.checkFirebaseToken = (token, callback) => { 
     admin.auth()
     .verifyIdToken(token)
     .then((decodedToken) => {
-        console.log("Firebase decoded token: " + decodedToken.email);
+        console.log("Firebase decoded token mail: " + decodedToken.email);
+        callback(decodedToken.email, null);
     })
     .catch((error) => {
-        throw new Error
-        console.log(error);
+        callback(null, error);
     });
 };
 
-exports.checkFacebookToken = (token) => { 
+exports.checkFacebookToken = (token, callback) => { 
     const url = "https://graph.facebook.com/me?fields=name,email&access_token=" + token;
     logInfo("Facebook url: " + url);
 
     axios.get(url)
       .then((res) => {
-        logInfo("Result for Facebook request: " + res["data"]["email"]);
-      }).catch((err) => {
-        logError(err.response.data.detail);
+        logInfo("User email after Facebook request: " + res["data"]["email"]);
+        callback(res["data"]["email"], null);
+      }).catch((error) => {
+        callback(null, error);
       });
 };
